f4fb813ecb42fbb8afe3a6358c2227ee84699b8a
littletake <ritorut18@gmail.com>
2020-01-17 21:00:01 +0900

    vueとflaskでウェブ開発（途中）

---
+++ web_page/flask_vue_prac/backend/main.py
@@ -0,0 +1,24 @@
+# できていない
+
+from flask import Flask, render_template, jsonify
+from random import *
+
+app = Flask(__name__, 
+            static_folder = '../frontend/dist/static', 
+            template_folder = '../frontend/dist'
+            )
+
+@app.route('/', defaults={'path':''})
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
+
+@app.route('/<path:path>')
+def index(path):
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
---
+++ web_page/flask_vue_prac/frontend/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '@/components/Home'
+import About from '@/components/About'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
